549b1e8e9b3f8f064160469df99c8a7aeed44331
davidpilecek <78658446+davidpilecek@users.noreply.github.com>
2022-03-13 00:46:26 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,66 +4,56 @@ import time
 import cv2 as cv
 
 
-arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.1)
+arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=.1)
 time.sleep(2)   
 
-cap = cv.VideoCapture(0 + cv.CAP_DSHOW)  
+cap = cv.VideoCapture(2)
 
 center_left = 175
 center_right = 325
 
 lower_black = np.array([0, 0, 0])
-upper_black = np.array([255, 255, 20])
+upper_black = np.array([180, 255, 100])
 counter = 0
 
 ang1 = 0
 ang2 = 0
 
-#trackbar callback fucntion does nothing but required for trackbar
-def nothing(x):
-	pass
+diff_allow = 5
 
 
-diff_allow = 30
-
-#create a seperate window named 'settings' for trackbar
-cv.namedWindow('settings')
-cv.resizeWindow("settings", 640, 240)
-#create trackbar in 'settings' window with name lower/upper_black'
-
-cv.createTrackbar("lower_black", "settings", 0, 255, nothing)
-cv.createTrackbar("upper_black", "settings", 255, 255, nothing)
-
-lower_blacktest = int(cv.getTrackbarPos("lower_black","settings"))
-upper_blacktest = int(cv.getTrackbarPos("upper_black","settings"))
 
 while True:
+    
 
-
-    try:
-        _, frame = cap.read()
-        if(type(frame) == type(None)):
-            pass
-        else:
-            frame = cv.resize(frame, (500, 500))
-
+  
+    _, frame = cap.read()
+    
+    if(type(frame) == type(None)):
+        pass
+    else:
+        frame = cv.resize(frame, (500, 500))
         height = frame.shape[0]
         width = frame.shape[1]
 
-        frameHSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
-        blur = cv.GaussianBlur(frameHSV, (7,7), 5)
+  
+    
 
-        mask = cv.inRange(blur, lower_black, upper_black)
 
-    except Exception as e:
-        print(str(e))
+    frameHSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+    blur = cv.GaussianBlur(frameHSV, (7,7), 5)
+
+    mask = cv.inRange(blur, lower_black, upper_black)
+    
+
+    
 
     contours, hierarchy = cv.findContours(mask, cv.RETR_TREE ,cv.CHAIN_APPROX_NONE)
     
     cv.line(frame, (center_left, 500), (center_left, 0), (0, 0, 255), 10)
     cv.line(frame, (center_right, 500), (center_right, 0), (0, 0, 255), 10)
-
 
     if len(contours)>0:
         contour = max(contours, key = cv.contourArea)
@@ -82,10 +72,14 @@ while True:
         if cX < int(width / 2):
             phi = np.degrees(np.arctan((height - cY) / (int(width / 2) - cX )))
             
+
+        if phi != 0 :
+            cv.putText(frame, str(round(phi)), (center_right, height-50), cv.FONT_HERSHEY_PLAIN, 3, (255,255,255), 5)
+        
         counter+=1
         
-# pokud je momentalni deg o 50 vetsi, nez predchozi, neposilej ho, pokud se ale vyskytne 2x za sebou v range +-10
-# tak jej pust
+
+
 
         if counter % 2 == 1:
             ang1 = round(phi)
@@ -100,22 +94,36 @@ while True:
         
         print("DIFF ", diff)
 
-        if counter % 2 and diff<=50:
+        if counter % 2 and diff<=diff_allow:
+            arduino.write(b"<")
             arduino.write(bytes(str(ang1), 'utf-8'))
-            print("ANG1WRITE")
+            arduino.write(b">")
 
-        elif counter % 2 == 0 and diff<=50:
+            print("ANG1WRITE")
+            
+
+        elif counter % 2 == 0 and diff<=diff_allow:
+
+
+            arduino.write(b"<")
             arduino.write(bytes(str(ang2), 'utf-8'))
+            arduino.write(b">")
+          
             print("ANG2WRITE")
+            
         else:
             print("diff too large")
 
-        time.sleep(0.05)
 
         arduino_data = arduino.readline()      
-        print("arduino serial: ", arduino_data.decode('utf-8'))
         
+        try:
+            print("arduino serial: ", arduino_data.decode('utf-8'))
+        except:
+            pass
+
         cv.line(frame, (int(width / 2), height), (cX, cY), (255, 255, 0), 5)
+        
 
 
          
@@ -128,7 +136,7 @@ while True:
     except Exception as e:
         print(str(e))
  
-    if cv.waitKey(100) == ord('q'):
+    if cv.waitKey(1) == ord('q'):
         break
 
 
