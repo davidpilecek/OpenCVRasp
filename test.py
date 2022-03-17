09b7ae7760a12740c1722d232208d300c6c8272f
davidpilecek <78658446+davidpilecek@users.noreply.github.com>
2022-03-17 19:29:24 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,28 +4,29 @@ import time
 import cv2 as cv
 
 
-arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=.1)
+arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.1)
 time.sleep(2)   
 
-cap = cv.VideoCapture(2)
+cap = cv.VideoCapture(r"C:\Users\David\Documents\git\OpenCVNew\video.mp4")
 
 center_left = 175
 center_right = 325
 
 lower_black = np.array([0, 0, 0])
 upper_black = np.array([180, 255, 100])
+
+lower_other = np.array([94, 80, 50])
+upper_other = np.array([130, 255, 255])
+
+
 counter = 0
 
 ang1 = 0
 ang2 = 0
 
-diff_allow = 5
-
-
+diff_allow = 50
 
 while True:
-    
-
   
     _, frame = cap.read()
     
@@ -37,28 +38,40 @@ while True:
         width = frame.shape[1]
 
 
-  
-    
-
-
     frameHSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
-    blur = cv.GaussianBlur(frameHSV, (7,7), 5)
+    blur = cv.GaussianBlur(frameHSV, (3,3), 5)
 
-    mask = cv.inRange(blur, lower_black, upper_black)
+    mask_black = cv.inRange(blur, lower_black, upper_black)
+    
+    mask_other = cv.inRange(blur, lower_other, upper_other)
     
 
-    
+    contours_black, hierarchy_black = cv.findContours(mask_black, cv.RETR_TREE ,cv.CHAIN_APPROX_NONE)
 
-    contours, hierarchy = cv.findContours(mask, cv.RETR_TREE ,cv.CHAIN_APPROX_NONE)
+    contours_other, hierarchy_other = cv.findContours(mask_other, cv.RETR_TREE ,cv.CHAIN_APPROX_NONE)
+
+
     
     cv.line(frame, (center_left, 500), (center_left, 0), (0, 0, 255), 10)
     cv.line(frame, (center_right, 500), (center_right, 0), (0, 0, 255), 10)
 
-    if len(contours)>0:
-        contour = max(contours, key = cv.contourArea)
+
+
+
+    if len(contours_other) != 0:
+
+        contour = max(contours_other, key = cv.contourArea)
+        cv.drawContours(frame, contour, -1, (255, 0, 0), 3)
+
+
+    if len(contours_black)>0:
+
+        contour = max(contours_black, key = cv.contourArea)
+
         cv.drawContours(frame, contour, -1, (0, 255, 0), 5 )
-        if len(contours)>0:
+
+        if len(contours_black)>0:
             M = cv.moments(contour)
             if(M["m10"] !=0 and M["m01"] !=0 and M["m00"] !=0):
                 cX = int(M["m10"] / M["m00"])
@@ -78,57 +91,54 @@ while True:
         
         counter+=1
         
+         
+    else:
+        cX, cY = 0, 0
 
-
-
-        if counter % 2 == 1:
+    if counter % 2 == 1:
             ang1 = round(phi)
             print("ANG1 ", ang1)
 
-        elif counter % 2==0:
+    elif counter % 2==0:
             ang2 = round(phi)
             print("ANG2 ", ang2)
 
-        diff = np.abs(ang1 - ang2)
+    diff = np.abs(ang1 - ang2)
 
         
-        print("DIFF ", diff)
+    
+    print("DIFF ", diff)
 
-        if counter % 2 and diff<=diff_allow:
+    if counter % 2 and diff<=diff_allow:
             arduino.write(b"<")
             arduino.write(bytes(str(ang1), 'utf-8'))
+            arduino.write(b"*")
+            arduino.write(bytes(str(ang1+1), 'utf-8'))
             arduino.write(b">")
-
-            print("ANG1WRITE")
             
 
-        elif counter % 2 == 0 and diff<=diff_allow:
+    elif counter % 2 == 0 and diff<=diff_allow:
 
 
             arduino.write(b"<")
             arduino.write(bytes(str(ang2), 'utf-8'))
+            arduino.write(b"*")
+            arduino.write(bytes(str(ang2+1), 'utf-8'))
             arduino.write(b">")
-          
-            print("ANG2WRITE")
+
             
-        else:
+    else:
             print("diff too large")
 
 
-        arduino_data = arduino.readline()      
+    arduino_data = arduino.readline()      
         
-        try:
+    try:
             print("arduino serial: ", arduino_data.decode('utf-8'))
-        except:
-            pass
+    except:
+        pass
 
-        cv.line(frame, (int(width / 2), height), (cX, cY), (255, 255, 0), 5)
-        
-
-
-         
-    else:
-        cX, cY = 0, 0
+    cv.line(frame, (int(width / 2), height), (cX, cY), (255, 255, 0), 5)
         
     try:
         hori = np.concatenate((frame, frameHSV), axis=1)
